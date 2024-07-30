0724462f57014bdf33fcea6fcad75d22facbf536
Erin <erinrainabo35@gmail.com>
2024-07-30 20:51:10 +0400

    Create start and display stopwatch functions

---
+++ script.js
@@ -0,0 +1,52 @@
+const minutesElement = document.querySelector('.js-minutes')
+const secondsElement = document.querySelector('.js-seconds')
+const millisecondsElement = document.querySelector('.js-milliseconds')
+const startButton = document.querySelector('.js-start-button')
+const resetButton = document.querySelector('.js-reset-button')
+let milliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+let isPlaying = false
+let intervalId;
+
+const displayStopwatch = () => {
+  millisecondsElement.innerHTML = milliseconds.toString().padStart(2, '0')
+  secondsElement.innerHTML = seconds.toString().padStart(2, '0')
+  minutesElement.innerHTML = minutes.toString().padStart(2, '0')
+}
+
+const startStopwatch = () => {
+  resetStopwatch()
+  startButton.addEventListener('click', () => {
+    if (!isPlaying) {
+      isPlaying = true
+      startButton.innerHTML = 'Stop'
+
+      intervalId = setInterval(() => {
+        if (milliseconds <= 100) {
+          milliseconds++
+          displayStopwatch()
+        } 
+        
+        if (milliseconds === 100) {
+          milliseconds = 0
+          seconds++
+          displayStopwatch()
+        }
+  
+        if (seconds === 60) {
+          seconds = 0
+          minutes++
+          displayStopwatch()
+          console.log(minutes)
+        }
+      }, 10)
+    } else if (isPlaying) {
+      isPlaying = false
+      startButton.innerHTML = 'Start'
+      clearInterval(intervalId)
+    }
+  })
+}
+
+startStopwatch()
